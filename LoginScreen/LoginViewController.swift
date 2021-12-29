07446a90b63bbb44c734dcddacd3f34df84ddcb7
AlexeyB163 <sourceaisa@mail.ru>
2021-12-30 00:24:04 +0400

    logic app completed

--- LoginScreen/LoginViewController.swift
+++ LoginScreen/LoginViewController.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum dateUser: String {
+    case userName = "User"
+    case password = "1234"
+}
+
+
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
@@ -15,8 +21,52 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if verificationDate() {
+            guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+            welcomeVC.userName = userNameTF.text ?? ""
+        } else {
+            alertDateAction()
+        }
+    }
+    
+    @IBAction func forgotUserAction() {
+    }
+    @IBAction func forgotPasswordAction() {
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let loginVC = segue.source as? WelcomeViewController else { return }
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    
+    func alertDateAction() {
+        let alertController  = UIAlertController(
+            title: "Invalid login or password",
+            message: "Please, enter correct login and password",
+            preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(
+            title: "OK",
+            style: .default)
+            { _ in self.passwordTF.text = "" }
+        
+        alertController.addAction(alertAction)
+        present(alertController, animated: true)
+    }
+        
+    
+    private func verificationDate() -> Bool {
+        let user = userNameTF.text == dateUser.userName.rawValue
+        let password = passwordTF.text == dateUser.password.rawValue
+        
+        return user && password
+    }
+    
+   
 }
 
---
+++ LoginScreen/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogOutViewController.swift
+//  LoginScreen
+//
+//  Created by User on 29.12.2021.
+//
+
+import UIKit
+
+
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var welcomeUser: UILabel!
+    
+    @IBOutlet weak var imageUser: UIImageView!
+    
+    var userName = ""
+    
+    var greetingUser: String {
+        return "Welcome" + " " + userName + " " + "!"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeUser.text = greetingUser
+        imageUser.image = UIImage(named: "user")
+        imageUser.layer.cornerRadius = imageUser.layer.frame.height/2
+    }
+}
